1364686cc12fcad4b64779cf78bb1dfe7a2d38e6
Steve <steve.jevon.r@gmail.com>
2025-09-26 17:16:56 +0800

    add a skiplist struct

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,27 @@ use std::cell::RefCell;
 fn main() {
     println!("Hello, world!");
 }
+
+struct SkipList{
+    max_level:i32,
+    p:132,
+    starter:Node
+}
+
+impl SkipList{
+    fn get_max_level(&self)->i32{
+        self.max_level
+    }
+    fn add_node(&self, id:i32, payload:string)->Option<&Node>{
+    }
+    fn gen_random_level(&self)->i32{
+        let lvl:i32 = 1
+        while rand::thread_rng().gen_range(0..100) < self.p && lvl<self.max_level{
+            level+=1
+        }
+        return lvl
+    }
+}
 #[derive(Clone, Debug)]
 struct Node<T: Eq + >{
     id:i32,
@@ -17,13 +38,6 @@ struct Node<T: Eq + >{
 }
 
 impl Node{
-    fn gen_random_level(&self, p:i32, max_level:i32)->i32{
-        let lvl:i32 = 1
-        while rand::thread_rng().gen_range(0..100) < p && lvl<max_level{
-            level+=1
-        }
-        return lvl
-    }
     fn add(&self, id:i32, payload:string)->Option<&Node>{
         let lvl:132 = self.gen_random_level()
         i   
